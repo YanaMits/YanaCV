7cbc219585d313d53b2843d74391f8f045cbeac3
Yana Mitskevich <yana.pressa@gmail.com>
2021-12-05 20:52:10 +0100

    min uppgift

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿function send() {
+    let nameInput = document.getElementById("name");
+    let companyInput = document.getElementById("company-name");
+    let emailInput = document.getElementById("email");
+
+    let newText = document.getElementById("new-text");
+    if (newText == null) {
+        newText = document.createElement("p");
+        newText.id = "new-text";
+        const footer = document.querySelector("footer");
+        footer.appendChild(newText); 
+    }
+
+    if (nameInput.value === "" || companyInput.value === "" || emailInput.value === "") {
+        newText.textContent = "Oops! Incorrect. Please, try again.";
+    }
+    else {
+        newText.textContent = nameInput.value + ", " + companyInput.value + ", " + "Tack att du försökte anställa mig!"
+        nameInput.value = "";
+        companyInput.value = "";
+        emailInput.value = "";
+    }
+        
+
+}
